9f7063747bbd6d9940fbe1911d610b2fb2baf457
nigusgetif <nigusgetif48@gmail.com>
2025-03-31 12:17:42 +0300

    rendering arrays

--- my-app/src/Header.jsx
+++ my-app/src/Header.jsx
@@ -1,7 +1,20 @@
 export default function Header(){
+    const nameLists =[
+        <h2>Bob</h2>,
+        <h2>Peter</h2>,
+        <h2>Tom</h2>,
+        <h2>Bek</h2>,
+        <h2>Dane</h2>,
+    ]
     return(
+        <>
         <header>
-             <img src="/src/assets/reactLogo.png"  width ="40px" alt="logo" />
+            <img src="/src/assets/reactLogo.png" width="40px" alt="logo" />
+
         </header>
+        <main>
+             {nameLists}
+         </main>
+     </>
     )
 }
